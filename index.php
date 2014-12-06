c4a2d0562bbac8e61b602b01db29738fefc37d5e
BlackLight <blacklight@autistici.org>
2014-12-06 19:56:21 +0100

    One more commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<html>
+
+<head>
+    <title>Spotify Player</title>
+
+    <!-- JS -->
+    <script language="javascript" type="text/javascript" src="static/js/lib/jquery-2.1.1.min.js"></script>
+    <script language="javascript" type="text/javascript" src="static/js/lib/bootstrap.min.js"></script>
+    <script language="javascript" type="text/javascript" src="static/js/lib/handlebars-v2.0.0.js"></script>
+    <script language="javascript" type="text/javascript" src="static/js/lib/annyang.min.js"></script>
+    <script language="javascript" type="text/javascript" src="static/js/app.js"></script>
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="static/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" src="static/css/style.css">
+
+    <!-- Templates -->
+    <?php
+	   include('templates/header.comp');
+    ?>
+</head>
+
+<body>
+    <div id="header"></div>
+    <div class="row">
+	   <div class="col-xs-12 col-sm-12 col-md-8 col-lg-8">
+		  <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+			 <iframe src="https://embed.spotify.com/?uri=spotify:track:4th1RQAelzqgY7wL53UGQt"
+				width="100%" height="100%"
+				frameborder="0" allowtransparency="true">
+			 </iframe>
+		  </div>
+	   </div>
+</body>
+</html>
+
--- static/js/app.js
+++ static/js/app.js
@@ -1,54 +1,65 @@
 $(document).ready(function() {
-if (annyang) {
-    // Let's define our first command. First the text we expect, and then the function it should call
-    var commands = {
-	   'play (the song) *song': playSong,
-	   'play (the song) *song by *artist': playSong,
-	   'stop': stop
+    var init = function() {
+	   initHeader();
+
+	   if (annyang) {
+		  // Let's define our first command. First the text we expect, and then the function it should call
+		  var commands = {
+			 'play (the song) *song': playSong,
+			 'play (the song) *song by *artist': playSong,
+			 'stop': stop
+		  };
+
+		  var audio = null;
+		  function playSong(song, artist) {
+			 var recognizedElement = document.getElementById('recognized');
+			 recognizedElement.innerText = 'Recognized "' + song + (artist ? ' by ' + artist : '') + '"';
+			 console.log("PlaySong", song);
+			 var req = new XMLHttpRequest();
+			 req.open('GET', 'https://api.spotify.com/v1/search?type=track&q=' + encodeURIComponent(song), true);
+
+			 req.onreadystatechange = function() {
+				if (req.readyState == 4 && req.status == 200){
+				    var data = JSON.parse(req.responseText);
+				    if (data.tracks.items[0]) {
+					   stop();
+					   var matchedElement = document.getElementById('matched');
+					   matchedElement.innerHTML = '<div class="media"><img src="' + data.tracks.items[0].album.images[0].url + '" alt="Cover art of ' + data.tracks.items[0].album.name + '" width="300"><p>Playing ' + data.tracks.items[0].name + ' by ' + data.tracks.items[0].artists[0].name +'</p></div>';
+					   audio = new Audio(data.tracks.items[0].preview_url);
+					   audio.play();
+				    }
+				}
+			 };
+			 req.send(null);
+		  }
+
+		  function stop() {
+			 if (audio) {
+				audio.pause();
+				audio = null;
+			 }
+		  } 
+
+		  function stop() {
+			 if (audio) {
+				audio.pause();
+			 }
+		  }    
+
+		  // Add our commands to annyang
+		  annyang.addCommands(commands);
+
+		  // Start listening. You can call this here, or attach this call to an event, button, etc.
+		  annyang.start();
+		  annyang.debug();
+	   }
     };
 
-    var audio = null;
-    function playSong(song, artist) {
-	   var recognizedElement = document.getElementById('recognized');
-	   recognizedElement.innerText = 'Recognized "' + song + (artist ? ' by ' + artist : '') + '"';
-	   console.log("PlaySong", song);
-	   var req = new XMLHttpRequest();
-	   req.open('GET', 'https://api.spotify.com/v1/search?type=track&q=' + encodeURIComponent(song), true);
+    var initHeader = function() {
+	   var header = Handlebars.compile($("#header-template").html());
+	   $("#header").html(header);
+    };
 
-	   req.onreadystatechange = function() {
-		  if (req.readyState == 4 && req.status == 200){
-			 var data = JSON.parse(req.responseText);
-			 if (data.tracks.items[0]) {
-				stop();
-				var matchedElement = document.getElementById('matched');
-				matchedElement.innerHTML = '<div class="media"><img src="' + data.tracks.items[0].album.images[0].url + '" alt="Cover art of ' + data.tracks.items[0].album.name + '" width="300"><p>Playing ' + data.tracks.items[0].name + ' by ' + data.tracks.items[0].artists[0].name +'</p></div>';
-				audio = new Audio(data.tracks.items[0].preview_url);
-				audio.play();
-			 }
-		  }
-	   };
-	   req.send(null);
-    }
-
-    function stop() {
-	   if (audio) {
-		  audio.pause();
-		  audio = null;
-	   }
-    } 
-
-    function stop() {
-	   if (audio) {
-		  audio.pause();
-	   }
-    }    
-
-    // Add our commands to annyang
-    annyang.addCommands(commands);
-
-    // Start listening. You can call this here, or attach this call to an event, button, etc.
-    annyang.start();
-    annyang.debug();
-}
+    init();
 });
 
