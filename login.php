3bee877445b08db3af4aec9b22164bb0bbaa95cf
BlackLight <blacklight@autistici.org>
2014-12-14 11:14:48 +0100

    Changed redirect URI

--- login.php
+++ login.php
@@ -4,7 +4,7 @@ require_once "google-api-php-client/autoload.php";
 
 const CLIENT_ID = "984540381166-u3bv62sb6ggppljn77dsue93m9b4fl3j.apps.googleusercontent.com";
 const CLIENT_SECRET = "iBpvjqJK1YHZDP4qNzMtnqwF";
-const REDIRECT_URI = "./oauth2.php";
+const REDIRECT_URI = "http://blacklight.gotdns.org/CodeSterix/oauth2.php";
 
 $client = new Google_Client();
 $client->setClientId(CLIENT_ID);
