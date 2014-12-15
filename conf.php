57e1feb6720a29506bd695c49dd840bfcf44b8b7
Fabio Manganiello <blacklight86@gmail.com>
2014-12-15 10:08:24 +0000

    Moved private credentials to conf_secret.php, to be git-ignored

--- conf.php
+++ conf.php
@@ -1,17 +1,17 @@
 <?php
 
+require_once "conf_secret.php";
+
 /* Database */
 const DB_HOST = "localhost";
 const DB_PORT = 3306;
 const DB_USER = "root";
-const DB_PASS = "";
 const DB_NAME = "tonlist";
 
 /* Google API */
 
 const TONLIST_URI = "/CodeSterix";
 const CLIENT_ID = "984540381166-u3bv62sb6ggppljn77dsue93m9b4fl3j.apps.googleusercontent.com";
-const CLIENT_SECRET = "iBpvjqJK1YHZDP4qNzMtnqwF";
 const LOGIN_URI = TONLIST_URI . "/login.php";
 const REDIRECT_URI = "http://localhost/" . TONLIST_URI . "/login.php";
 
---
+++ conf_secret.php
@@ -0,0 +1,9 @@
+<?php
+
+/* Database */
+const DB_PASS = "";
+
+/* Google API */
+const CLIENT_SECRET = "iBpvjqJK1YHZDP4qNzMtnqwF";
+
+?>
