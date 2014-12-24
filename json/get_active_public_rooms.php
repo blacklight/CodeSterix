3cafecba43ddb338ca451a856496c60476019cc1
BlackLight <blacklight@autistici.org>
2014-12-24 10:35:07 +0100

    Returning the users active on a certain room

--- json/get_active_public_rooms.php
+++ json/get_active_public_rooms.php
@@ -2,6 +2,7 @@
 
 require_once "../conf.php";
 require_once TONLIST_PATH . "/lib/db/db_room.php";
+require_once TONLIST_PATH . "/lib/db/db_user_room.php";
 require_once TONLIST_PATH . "/lib/db/db_user.php";
 
 header('Content-Type: application/json');
@@ -20,6 +21,15 @@ while ($room = $stmt->fetchObject()) {
 		  "name"    => $user->name,
 		  "picture" => $user->picture,
 	   );
+    }
+
+    $user_stmt = $_DB["user_room"]->search_where(array(
+	   "room_id" => $room->id
+    ));
+
+    $room->users = array();
+    while ($user = $user_stmt->fetchObject()) {
+	   array_push($room->users, $user);
     }
 
     array_push($rooms, $room);
