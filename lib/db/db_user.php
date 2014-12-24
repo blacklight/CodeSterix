22de7f282028057b377ab3234a8555eaf7e3e300
BlackLight <blacklight@autistici.org>
2014-12-24 14:40:50 +0100

    Backend logic for supporting user disconnections

--- lib/db/db_user.php
+++ lib/db/db_user.php
@@ -1,6 +1,10 @@
 <?php
 
-require_once "db.php";
+require_once "db_room.php";
+require_once "db_user_room.php";
+require_once "db_user_room_history.php";
+
+global $_DB;
 
 class DbUser extends Db {
     protected $table_name = "tonlist_user";
@@ -14,6 +18,62 @@ class DbUser extends Db {
     );
 
     protected $primary_key = "id";
+
+    public function disconnect($user_id, $new_room_id) {
+	   global $_DB;
+
+	   if (!isset($new_room_id)) {
+		  $this->query("UPDATE " . $this->table_name
+			 . " SET logged_in = 0 WHERE id = ?",
+			 $user_id);
+	   }
+
+	   $rooms = $_DB["user_room"]->search_where(array(
+		  "user_id" => $user_id
+	   ));
+
+	   while ($user_room = $rooms->fetchObject()) {
+		  if (isset($new_room_id) && $user_room->room_id == $new_room_id) {
+			 continue;
+		  }
+
+		  $this->query("DELETE FROM " . $_DB["user_room"]->get_table_name()
+			 . " WHERE user_id = ? AND room_id = ?",
+			 $user_id, $user_room->room_id);
+
+		  $user_cnt = $_DB["user_room"]->query("SELECT COUNT(*) AS cnt
+			 FROM " . $_DB["user_room"]->get_table_name() . "
+			 WHERE room_id = ?",
+			 $user_room->room_id);
+
+		  $user_cnt = $user_cnt->fetchObject();
+		  if ($user_cnt->cnt == 0) {
+			 $_DB["room"]->query("DELETE FROM " . $_DB["room"]->get_table_name() .
+				" WHERE id = ?",
+				$user_room->room_id);
+		  }
+	   }
+    }
+
+    public function enter_room($args) {
+	   global $_DB;
+
+	   $this->disconnect($args["user_id"], $args["room_id"]);
+
+	   $this->query("UPDATE " . $this->table_name
+		  . " SET logged_in = 1 WHERE id = ?",
+		  $args["user_id"]);
+
+	   $_DB["user_room"]->insert_ignore(array(
+		  "user_id" => $args["user_id"],
+		  "room_id" => $args["room_id"],
+	   ));
+
+	   $_DB["user_room_history"]->insert_ignore(array(
+		  "user_id" => $args["user_id"],
+		  "room_id" => $args["room_id"],
+	   ));
+    }
 }
 
 $_DB["user"] = new DbUser();
--- lib/db/db_user_room.php
+++ lib/db/db_user_room.php
@@ -1,7 +1,6 @@
 <?php
 
-require_once "db_user_room_history.php";
-global $_DB;
+require_once "db.php";
 
 class DbUserRoom extends Db {
     protected $table_name = "tonlist_user_room";
@@ -12,23 +11,6 @@ class DbUserRoom extends Db {
     );
 
     protected $primary_key = ["user_id", "room_id"];
-
-    public function enter_room($args) {
-	   global $_DB;
-
-	   $this->query("DELETE FROM " . $this->table_name . " WHERE user_id = ? AND room_id = ?",
-		  $args["user_id"], $args["room_id"]);
-
-	   $this->insert(array(
-		  "user_id" => $args["user_id"],
-		  "room_id" => $args["room_id"],
-	   ));
-
-	   $_DB["user_room_history"]->insert(array(
-		  "user_id" => $args["user_id"],
-		  "room_id" => $args["room_id"],
-	   ));
-    }
 }
 
 $_DB["user_room"] = new DbUserRoom();
